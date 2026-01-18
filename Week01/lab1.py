b2ec998dfa926a2e110dc2ff827457df91cb9b25
JDTeddy <jaredduldulao05@gmail.com>
2026-01-18 03:20:56 -0500

    add lab1

---
+++ Week01/lab1.py
@@ -0,0 +1,26 @@
+# Sample Coding Question 01 Week 01
+# Jared-Ian Duldulao
+
+
+# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+
+# Question 3:
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d)) + (a % c)
+print("Question 3 result:", e)
+
+# Question 4:
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Question 5:
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtred by age:", userAge)
---
+++ Week01/q5.py
@@ -0,0 +1,4 @@
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtred by age:", userAge)
