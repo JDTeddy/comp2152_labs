a5cf07c3d475f876e9a420484420df2972a802aa
JDTeddy <jaredduldulao05@gmail.com>
2026-01-25 02:26:32 -0500

    add week 02 solution

---
+++ Week02/Lab02.py
@@ -0,0 +1,22 @@
+import random
+choices = ["Rock", "Paper", "Scissors"]
+
+playerChoice = input("Choose a number between the following list: 1-Rock, 2-Paper, 3-Scissors")
+
+playerChoice = int(playerChoice)
+
+if playerChoice < 1 or playerChoice > 3:
+    print("Error: Pleese choose a number between 1 and 3")
+else:
+    computerChoice = random.randint(1,3)
+
+    if playerChoice == computerChoice:
+        print("Tie!")
+    elif playerChoice == 1 and computerChoice == 3:
+        print("Rock beats Scissors - You Win!")
+    elif playerChoice == 2 and computerChoice == 1:
+        print("Paper beats Rock - You Win!")
+    elif playerChoice == 3 and computerChoice == 2:
+        print("Scissors beats Paper - You Win!")
+    else:
+        print("You lose!")
